37b280a732d0891939be81b6f0680cb9be412b76
Anto_Anisten <143519879+Anto0111@users.noreply.github.com>
2023-08-31 23:56:37 +0530

    Add files via upload

---
+++ sun and difference of two numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a=10;
+    int b=4;
+    float c=4.0;
+    float d=2.0;
+    
+    printf("%d %d", a+b ,a-b);
+    printf("\n%.1f %.1f", c+d ,c-d);
+    
+    return 0;
+}
