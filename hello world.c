534028d80db2c11c9becdb5f6a3a4e479748e7a8
Anto_Anisten <143519879+Anto0111@users.noreply.github.com>
2023-08-31 23:52:59 +0530

    Create hello world.c

---
+++ hello world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100]="Welcome to C programming.";
+    
+    printf("Hello, World!");
+    printf("\n%s",s);
+  	   
+    return 0;
+}
